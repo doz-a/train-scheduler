4a25765a3c64015183150d77b5124e40936a9ac0
Miguel Delos Santos <mendoza.d.santos@gmail.com>
2019-11-22 17:45:23 -0800

    Main containers for the HTML finished, firebase successfully storing objects in console

--- javascript/code.js
+++ javascript/code.js
@@ -1,93 +1,31 @@
-/* global firebase */
-
-// Initialize Firebase
-// Make sure that your configuration matches your firebase script version
-// (Ex. 3.0 != 3.7.1)
+// 1. Initialize Firebase
 var config = {
     apiKey: "AIzaSyC5kccnMrv77Ob-RHaNn7R5WeDzaNSYwXw",
     authDomain: "train-scheduler-468bc.firebaseapp.com",
     databaseURL: "https://train-scheduler-468bc.firebaseio.com",
+    projectId: "train-scheduler-468bc",
     storageBucket: "train-scheduler-468bc.appspot.com",
-    messagingSenderId: "259733638640"
+    messagingSenderId: "259733638640",
+    appId: "1:259733638640:web:4448610fe473da8b2ca135",
 };
 
 firebase.initializeApp(config);
 
-// Create a variable to reference the database
 var database = firebase.database();
 
-// Use the below initialValue
-var initialValue = 10;
+database.ref().on("child_added", function (childSnapshot) {
+    console.log(childSnapshot.val());
 
-// Use the below variable clickCounter to keep track of the clicks.
-var clickCounter = initialValue;
+    // Store everything into a variable.
+    var trainName = childSnapshot.val().name;
+    var destination = childSnapshot.val().role;
+    var frequency = childSnapshot.val().start;
+    var arrival = childSnapshot.val().rate;
 
-// --------------------------------------------------------------
+    // Employee Info
+    console.log(trainName);
+    console.log(destination);
+    console.log(frequency);
+    console.log(arrival);
 
-// At the initial load and on subsequent data value changes, get a snapshot of the current data. (I.E FIREBASE HERE)
-// This callback keeps the page updated when a value changes in firebase.
-database.ref().on("value", function (snapshot) {
-    // We are now inside our .on function...
-
-    // Console.log the "snapshot" value (a point-in-time representation of the database)
-    console.log(snapshot.val());
-    // This "snapshot" allows the page to get the most current values in firebase.
-
-    // Change the value of our clickCounter to match the value in the database
-    clickCounter = snapshot.val().clickCount;
-
-    if (clickCounter === 0) {
-        alert("Phew! You made it! That sure was a lot of clicking.");
-    }
-    // Console Log the value of the clickCounter
-    console.log(clickCounter);
-
-    // Change the HTML using jQuery to reflect the updated clickCounter value
-    $("#click-value").text(clickCounter);
-    // Alternate solution to the above line
-    // $("#click-value").html(clickCounter);
-
-    // If any errors are experienced, log them to console.
-}, function (errorObject) {
-    console.log("The read failed: " + errorObject.code);
-});
-
-// --------------------------------------------------------------
-
-// Whenever a user clicks the click button
-$("#click-button").on("click", function () {
-
-    // Reduce the clickCounter by 1
-    clickCounter--;
-
-    // Alert User and reset the counter
-
-    if (clickCounter < 0) {
-        clickCounter = initialValue;
-    }
-    // Save new value to Firebase
-    database.ref().set({
-        clickCount: clickCounter
-    });
-
-    // Log the value of clickCounter
-    console.log(clickCounter);
-
-});
-
-// Whenever a user clicks the restart button
-$("#restart-button").on("click", function () {
-
-    // Set the clickCounter back to initialValue
-    clickCounter = initialValue;
-
-    // Save new value to Firebase
-    database.ref().set({
-        clickCount: clickCounter
-    });
-
-    // Log the value of clickCounter
-    console.log(clickCounter);
-
-
-});
+})
