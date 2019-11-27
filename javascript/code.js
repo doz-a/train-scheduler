d24dfbb890dff025a809e2f9c99f02400f45670e
Miguel Delos Santos <mendoza.d.santos@gmail.com>
2019-11-27 11:08:36 -0800

    Working on a branch, grabbing user input successfully console logged

--- javascript/code.js
+++ javascript/code.js
@@ -13,19 +13,69 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
+// Start click event to add trains
+$('#add-train-btn').on('click', function (event) {
+    event.preventDefault();
+
+    // User input for trains 
+    var trainName = $('#train-name-input').val().trim();
+    console.log(trainName + ' input');
+    var destination = $('#destination-input').val().trim();
+    console.log(destination + ' input');
+    var arrival = $('#first-time-input').val().trim();
+    console.log(arrival + ' input');
+    var frequency = $('#frequency-input').val().trim();
+    console.log(frequency + ' input');
+
+});
+
+// Firebase event for adding trains to the database 
 database.ref().on("child_added", function (childSnapshot) {
     console.log(childSnapshot.val());
 
     // Store everything into a variable.
     var trainName = childSnapshot.val().name;
-    var destination = childSnapshot.val().role;
-    var frequency = childSnapshot.val().start;
-    var arrival = childSnapshot.val().rate;
+    var destination = childSnapshot.val().destination;
+    var arrival = childSnapshot.val().arrival;
+    var frequency = childSnapshot.val().frequency;
 
-    // Employee Info
+    // Train info
     console.log(trainName);
     console.log(destination);
-    console.log(frequency);
     console.log(arrival);
+    console.log(frequency);
 
-})
+    // Start Train Time Calculations 
+    // Assumptions (in minutes)
+    // var tFrequency = 3;
+
+    // // Time is 3:30 AM
+    // var firstTime = "03:30";
+
+    // // First Time (pushed back 1 year to make sure it comes before current time)
+    // var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+    // console.log(firstTimeConverted);
+
+    // // Current Time
+    // var currentTime = moment();
+    // console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+    // // Difference between the times
+    // var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    // console.log("DIFFERENCE IN TIME: " + diffTime);
+
+    // // Time apart (remainder)
+    // var tRemainder = diffTime % tFrequency;
+    // console.log(tRemainder);
+
+    // // Minute Until Train
+    // var tMinutesTillTrain = tFrequency - tRemainder;
+    // console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+    // // Next Train
+    // var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    // console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
+    // End train time calculations 
+
+});
