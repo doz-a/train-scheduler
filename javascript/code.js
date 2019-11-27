9efc8df4f5b8e5894fa2cf95e0db3259517bc15d
Miguel Delos Santos <mendoza.d.santos@gmail.com>
2019-11-27 12:12:08 -0800

    console log of train time calculations successful

--- javascript/code.js
+++ javascript/code.js
@@ -39,7 +39,7 @@ $('#add-train-btn').on('click', function (event) {
 
     // New train object gets uploaded to database 
     database.ref().push(newTrain);
-    console.log("new train successfully uploaded to database");
+    // console.log("new train successfully uploaded to database");
 
     // Clears text boxes to add more trains 
     $('#train-name-input').val('');
@@ -51,7 +51,7 @@ $('#add-train-btn').on('click', function (event) {
 
 // Firebase event for adding trains to the database 
 database.ref().on("child_added", function (childSnapshot) {
-    console.log(childSnapshot.val());
+    // console.log(childSnapshot.val());
 
     // Store everything into a variable.
     var trainName = childSnapshot.val().trainNameX;
@@ -60,37 +60,39 @@ database.ref().on("child_added", function (childSnapshot) {
     var frequency = childSnapshot.val().frequencyX;
 
     // Train info
-    console.log(trainName);
-    console.log(destination);
-    console.log(arrival);
-    console.log(frequency);
+    // console.log(trainName);
+    // console.log(destination);
+    // console.log(arrival);
+    // console.log(frequency);
 
     // Start Train Time Calculations 
     // Assumptions (in minutes)
-    // var tFrequency = 3;
+    // var is frequency 
+    var tFrequency = 3;
 
     // // Time is 3:30 AM
-    // var firstTime = "03:30";
+    // var is arrival 
+    var firstTime = "03:30";
 
     // // First Time (pushed back 1 year to make sure it comes before current time)
-    // var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
-    // console.log(firstTimeConverted);
+    var firstTimeConverted = moment(firstTime, "HHmm").subtract(1, "years");
+    console.log(firstTimeConverted);
 
-    // // Current Time
-    // var currentTime = moment();
-    // console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+    // // Gets the current time in MILITARY time
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("HHmm"));
 
     // // Difference between the times
-    // var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-    // console.log("DIFFERENCE IN TIME: " + diffTime);
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
 
     // // Time apart (remainder)
-    // var tRemainder = diffTime % tFrequency;
-    // console.log(tRemainder);
+    var tRemainder = diffTime % tFrequency;
+    console.log(tRemainder);
 
     // // Minute Until Train
-    // var tMinutesTillTrain = tFrequency - tRemainder;
-    // console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+    var tMinutesTillTrain = tFrequency - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
 
     // // Next Train
     // var nextTrain = moment().add(tMinutesTillTrain, "minutes");
@@ -99,7 +101,7 @@ database.ref().on("child_added", function (childSnapshot) {
     // End train time calculations 
 
     // Create the new row in the train schedule table
-    // Train Name, Destination, First train TimeRanges, frequency, minutes away 
+    // Train Name, Destination, First train TimeRanges, frequency, minutes away (calculated)
 
     var newRow = $("<tr>").append(
         $("<td>").text(trainName),
@@ -109,7 +111,7 @@ database.ref().on("child_added", function (childSnapshot) {
         $("<td>").text("placeholder for time calculation")
     );
 
-    // Append the new row to the table
+    // Append the row to the table
     $("#train-table > tbody").append(newRow);
 
 });
